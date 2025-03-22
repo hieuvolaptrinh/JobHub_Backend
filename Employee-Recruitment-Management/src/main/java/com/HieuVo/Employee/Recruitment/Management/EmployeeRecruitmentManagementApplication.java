ee0a2585a99bfaa4ad4fb247c62243a7b4b04174
hieuvolaptrinh <vndhieuak@gmail.com>
2025-03-23 00:32:00 +0700

    first

---
+++ Employee-Recruitment-Management/src/main/java/com/HieuVo/Employee/Recruitment/Management/EmployeeRecruitmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.HieuVo.Employee.Recruitment.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRecruitmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRecruitmentManagementApplication.class, args);
+	}
+
+}
---
+++ Employee-Recruitment-Management/src/test/java/com/HieuVo/Employee/Recruitment/Management/EmployeeRecruitmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HieuVo.Employee.Recruitment.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRecruitmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
