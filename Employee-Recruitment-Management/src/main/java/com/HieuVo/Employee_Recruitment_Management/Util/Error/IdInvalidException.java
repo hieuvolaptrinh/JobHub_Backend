dc4e228f46bfc4fc04efb1347643dc2bbd6a66ff
hieuvolaptrinh <vndhieuak@gmail.com>
2025-04-08 05:49:47 +0700

    rename

--- Employee-Recruitment-Management/src/main/java/com/HieuVo/Employee_Recruitment_Management/Util/Error/IdInvalidException.java
+++
@@ -1,9 +0,0 @@
-package com.HieuVo.Employee_Recruitment_Management.Util.Error;
-
-
-public class IdInvalidException extends  Exception {
-
-    public IdInvalidException(String message) {
-        super(message);
-    }
-}
